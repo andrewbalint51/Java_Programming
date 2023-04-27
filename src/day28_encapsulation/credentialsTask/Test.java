f497ba58005b915412e44dd2173af371e122240f
Andrew Balint <Andrew.balint@hotmail.com>
2023-04-27 08:51:17 -0700

    Adding Second Class to commit for practice

---
+++ src/day28_encapsulation/credentialsTask/Test.java
@@ -0,0 +1,11 @@
+package day28_encapsulation.credentialsTask;
+
+public class Test {
+    public static void main(String[] args) {
+        Credentials cred = new Credentials("andrewb", "Biff@or7d");
+
+        System.out.println(cred);
+    }
+
+
+}
